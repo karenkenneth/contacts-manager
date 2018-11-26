a7654436bee87531c6d999aaa68611d69ee3b62f
KennethMcInturf <kennethcmcinturf@gmail.com>
2018-11-26 15:01:44 -0600

    Base Functionaity complete

---
+++ src/Input.java
@@ -0,0 +1,153 @@
+import java.util.Scanner;
+
+public class Input {
+
+    private Scanner sc;
+
+    public Input(){
+        this.sc = new Scanner(System.in);
+    }
+
+    public String getString(){
+        return this.sc.nextLine();
+    }
+
+    public String getString(String prompt){
+        System.out.println(prompt);
+        return this.sc.nextLine();
+    }
+
+    public Boolean yesNO(){
+        return this.sc.nextLine().equals("yes");
+    }
+
+    public Boolean yesNO(String prompt){
+        System.out.println(prompt);
+        return this.sc.nextLine().equals("yes");
+    }
+
+    public Integer getInt(){
+        try {
+            return Integer.valueOf(this.sc.nextLine());
+        }catch (NumberFormatException e){
+            System.out.println("Please enter a Interger in number format");
+            return getInt();
+        }
+    }
+
+
+    public Integer getInt(String prompt){
+        try {
+            System.out.println(prompt);
+            return Integer.valueOf(this.sc.nextLine());
+        }catch (NumberFormatException e){
+            System.out.println("Please enter a Interger in number format");
+            return getInt(prompt);
+        }
+    }
+
+    public Integer getInt(int min, int max){
+        int number;
+        try {
+            number = Integer.valueOf(this.sc.nextLine());
+        }catch (NumberFormatException e){
+            System.out.println("Please enter a Interger in number format");
+            return getInt(min,max);
+        }
+        if (number <= max && number >= min){
+            return number;
+        }else {
+            System.out.println("Number not in range, please enter a new number.");
+            return getInt(min,max);
+        }
+    }
+
+    public Integer getInt(int min, int max, String prompt){
+        System.out.println(prompt);
+        int number;
+        try {
+            number = Integer.valueOf(this.sc.nextLine());
+        }catch (NumberFormatException e){
+            System.out.println("Please enter a Interger in number format");
+            return getInt(min,max);
+        }
+        if (number <= max && number >= min){
+            return number;
+        }else {
+            System.out.println("Number not in range, please enter a new number.");
+            return getInt(min,max);
+        }
+    }
+
+    public Double getDouble(){
+        try {
+            return Double.valueOf(this.sc.nextLine());
+        }catch (NumberFormatException e){
+            System.out.println("Please enter a Double in number format");
+            return getDouble();
+        }
+    }
+
+    public Double getDouble(String prompt){
+        System.out.println(prompt);
+        try {
+            return Double.valueOf(this.sc.nextLine());
+        }catch (NumberFormatException e){
+            System.out.println("Please enter a Double in number format");
+            return getDouble();
+        }
+    }
+
+    public Double getDouble(int min, int max){
+        double number;
+        try {
+            number = Double.valueOf(this.sc.nextLine());
+        }catch (NumberFormatException e){
+            System.out.println("Please enter a double in number format");
+            return getDouble(min,max);
+        }
+        if (number <= max && number >= min){
+            return number;
+        }else {
+            System.out.println("Number not in range, please enter a new number.");
+            return getDouble(min,max);
+        }
+    }
+
+    public Double getDouble(int min, int max, String prompt){
+        System.out.println(prompt);
+        double number;
+        try {
+            number = Double.valueOf(this.sc.nextLine());
+        }catch (NumberFormatException e){
+            System.out.println("Please enter a double in number format");
+            return getDouble(min,max);
+        }
+        if (number <= max && number >= min){
+            return number;
+        }else {
+            System.out.println("Number not in range, please enter a new number.");
+            return getDouble(min,max);
+        }
+    }
+
+    public int getBinary(){
+        try {
+            String binary = this.sc.next();
+            return Integer.parseInt(binary, 2);
+        }catch (NumberFormatException e){
+            System.out.println("Please enter a valid number");
+            return getBinary();
+        }
+    }
+
+    public int getHex(){
+        try {
+            String hex = this.sc.nextLine();
+            return Integer.parseInt(hex, 16);
+        }catch (NumberFormatException e){
+            System.out.println("Please enter a valid number");
+            return getHex();
+        }
+    }
+}
--- src/contactsManager.java
+++ src/contactsManager.java
@@ -1,3 +1,144 @@
-public class contactsManager {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class contactsManager {
+    public static void main(String[] args) {
+        mainMenu();
+    }
+
+    public static void mainMenu(){
+        Input input = new Input();
+        Boolean continueLoop = true;
+        do {
+            options();
+            int userInput = input.getInt(1,5);
+            continueLoop = userInteraction(userInput);
+        }while (continueLoop);
+        System.out.println("Thanks for using this");
+    }
+
+    public static boolean userInteraction(int userInput){
+        if (userInput == 1){
+            retriveContact();
+            return true;
+        }else if(userInput == 2){
+            createDirectory();
+            return true;
+        }else if (userInput == 3){
+            searchContact();
+            return true;
+        }else if(userInput == 4){
+            removeContact();
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public static void options() {
+        System.out.println("1. View contacts.\n");
+        System.out.println("2. Add a new contact.\n");
+        System.out.println("3. Search a contact by name.\n");
+        System.out.println("4. Delete an existing contact.\n");
+        System.out.println("5. Exit.\n");
+        System.out.println("Enter an option (1, 2, 3, 4 or 5):\n");
+    }
+
+    public static void displayContacts(){
+        System.out.println("Name | Phone number\n");
+        System.out.println("---------------\n");
+        retriveContact();
+    }
+
+    public static void removeContact(){
+        Input input = new Input();
+        String contactToDelete = input.getString("Enter name of contact to delete(first and last)");
+        deleteContact(contactToDelete);
+    }
+
+    public static void deleteContact(String contactToDelete){
+        Path filePath = Paths.get("src/contacts.txt");
+        List<String> removeList = new ArrayList<>();
+        try {
+            removeList = Files.readAllLines(filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String contactToRemove = "";
+        for (String contact : removeList){
+            if (contact.startsWith(contactToDelete)){
+                contactToRemove = contact;
+            }
+        }
+        removeList.remove(contactToRemove);
+        try {
+            Files.write(Paths.get("src/contacts.txt"), removeList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void createDirectory(){
+        Input input = new Input();
+        String firstName = input.getString("Please Enter Contacts first name");
+        String  lastName = input.getString("Please Enter Contacts last name");
+        int telephoneNumberFirstDigit = input.getInt("Please enter contacts telephone number first three digits");
+        int telephoneNumberSecondDigit = input.getInt("Please enter contacts telephone number second three digits");
+        int telephoneNumberThirdDigit = input.getInt("Please enter contacts telephone number final four digits");
+        addContact(firstName + " " +  lastName + " | " +
+                telephoneNumberFirstDigit + "-" + telephoneNumberSecondDigit + "-" + telephoneNumberThirdDigit);
+    }
+
+    public static void addContact(String addInfo){
+        Path filePath = Paths.get("src/contacts.txt");
+        List<String> addedList = new ArrayList<>();
+        try {
+            addedList = Files.readAllLines(filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        addedList.add(addInfo);
+        try {
+            Files.write(Paths.get("src/contacts.txt"), addedList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void searchContact(){
+        Input input = new Input();
+        String searchedName = input.getString("Please enter the searched name");
+        Path filePath = Paths.get("src/contacts.txt");
+        List<String> lines = new ArrayList<>();
+        try {
+            lines = Files.readAllLines(filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (String line : lines){
+            if (line.startsWith(searchedName)){
+                System.out.println(line);
+            }
+        }
+    }
+
+
+    public static void retriveContact() {
+        Path filePath = Paths.get("src/contacts.txt");
+        List<String> lines = new ArrayList<>();
+        try {
+            lines = Files.readAllLines(filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (String line : lines){
+            System.out.println(line);
+        }
+    }
 }
